ae054627866df0c476abd46343f8ae539d39ab11
Reese Pollard <rpollard@gmail.com>
2022-07-23 19:56:42 -0700

    Add reset button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateDivGrid(divRows, divColumns) {
     let divRow;
     let divSquare;
 
-    for (i = 0;i < divRows;i++) {
+    for (i = 0; i < divRows; i++) {
         divRow = document.createElement('div');
         divRow.classList.add ('row');
         divRow.setAttribute('id', `row${i}`);
@@ -29,13 +29,59 @@ function generateDivGrid(divRows, divColumns) {
     }
 }
 
-generateDivGrid(16, 16);
+function clearGrid() {
+    squares.forEach((square) => square.remove());
+    rows.forEach((row) => row.remove());
+}
 
-const squares = document.querySelectorAll('.square')
-const rows = document.querySelectorAll('.square')
+function updateSquareSize(sideLength) {
+    squares.forEach((square) => square.style.height = sideLength + "px");
+    squares.forEach((square) => square.style.width = sideLength + "px");
 
-squares.forEach((square) => square.addEventListener('mouseover', () => {
-    square.classList.add('hover')
-}));
+    return sideLength;
+}
+
+function addSquareClass(square, className) {
+    square.classList.add(className);
+}
+
+function resetGrid(promptUser) {
+    let gridSize = 0;
+    if (promptUser) {
+        do { // Loop until valid answer
+            gridSize = parseInt(prompt("Grid elements per side [MAX 100]:")); 
+
+            if (gridSize === null) gridSize = 16;
+        } while (gridSize < 0 || gridSize > 100);
+    } else {
+        gridSize = 16;
+    }
+    //squares.forEach((square) => square.classList.remove('hover'));
+    clearGrid();
+
+    console.log(gridSize);
+    generateDivGrid(gridSize, gridSize);
+
+    // need to re-select
+    squares = document.querySelectorAll('.square');
+
+    updateSquareSize(GRID_X / gridSize);
+    squares.forEach((square) => square.addEventListener('mouseover', () => addSquareClass(square, 'hover')));
+
+
+}
+
+let squares;
+let rows;
+const resetButton = document.querySelector('#reset');
+
+squares = document.querySelectorAll('.square');
+rows = document.querySelectorAll('.row');
+
+resetGrid(false);
+
+squares.forEach((square) => square.addEventListener('mouseover', () => addSquareClass(square, 'hover')));
+resetButton.addEventListener('click', () => resetGrid(true));
+
 
 // within each of those 4 divs generate 4 divs
